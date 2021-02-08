b2690b8bcae14a039429e1551a7d6741e5339fb2
benberryallwood <b.berry-allwood@protonmail.com>
2021-02-08 18:07:33 +0000

    Uncomment some lines in main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from unittest import main
 
 food = budget.Category("Food")
 food.deposit(1000, "initial deposit")
-#food.withdraw(10.15, "groceries")
-#food.withdraw(15.89, "restaurant and more food for dessert")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
 print(food.get_balance())
 clothing = budget.Category("Clothing")
 food.transfer(50, clothing)
@@ -20,7 +20,7 @@ print(food)
 print(clothing)
 print(auto)
 
-#print(create_spend_chart([food, clothing, auto]))
+print(create_spend_chart([food, clothing, auto]))
 
 entertainment = budget.Category("entertainment")
 business = budget.Category("business")
